6d1d7ede1a8b202936b49bc935273464525dae64
Ilyas.Khakiev <xakslim@gmail.com>
2023-01-29 10:11:02 +0300

    решение задач. вопросы: в тертьей задаче: правильно ли присваивать данные в created, чтобы не получать ошибки "нет данных" у computed?

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,6 +1,6 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from "./vendor/vue.esm-browser.js";
 
-const API_URL = 'https://course-vue.javascript.ru/api';
+const API_URL = "https://course-vue.javascript.ru/api";
 
 function fetchMeetupById(meetupId) {
   return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
@@ -14,4 +14,37 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+  name: "App",
+  data() {
+    return {
+      meetups: null,
+      meetupId: null,
+      meetupTitle: null,
+    };
+  },
+
+  watch: {
+    meetupId(value) {
+      fetchMeetupById(value).then(response => {
+        this.meetupTitle = response.title
+      })
+    },
+  },
+
+  template: `
+    <div>
+    <label><input type="radio" name="meetupId" v-model="meetupId" value="1" /> 1</label>
+    <label><input type="radio" name="meetupId" v-model="meetupId" value="2" /> 2</label>
+    <label><input type="radio" name="meetupId" v-model="meetupId" value="3" /> 3</label>
+    <label><input type="radio" name="meetupId" v-model="meetupId" value="4" /> 4</label>
+    <label><input type="radio" name="meetupId" v-model="meetupId" value="5" /> 5</label>
+
+    <hr />
+
+    <h3>{{ meetupTitle }}</h3>
+    </div>`
+});
+const app = createApp(App);
+const vm = app.mount("#app");
+window.vm = vm;
