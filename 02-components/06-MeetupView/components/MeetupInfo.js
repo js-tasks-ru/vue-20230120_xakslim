c8f2c28374a788c481728d41c40aee444bc1be08
Ilyas.Khakiev <xakslim@gmail.com>
2023-01-31 20:11:12 +0300

    решение задач второго модуля

--- 02-components/06-MeetupView/components/MeetupInfo.js
+++ 02-components/06-MeetupView/components/MeetupInfo.js
@@ -12,7 +12,7 @@ export default defineComponent({
       required: true
     },
     date: {
-      type: String,
+      type: Number,
       required: true
     }
   },
--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,26 +1,64 @@
-import { defineComponent } from '../vendor/vue.esm-browser.js';
-import UiContainer from './UiContainer.js';
-import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { defineComponent } from "../vendor/vue.esm-browser.js";
+import UiContainer from "./UiContainer.js";
+import UiAlert from "./UiAlert.js";
+import { fetchMeetupById } from "../meetupService.js";
+import MeetupView from "../../06-MeetupView/components/MeetupView";
 
 export default defineComponent({
-  name: 'PageMeetup',
-
+  name: "PageMeetup",
   components: {
     UiAlert,
     UiContainer,
+    MeetupView
   },
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      meetup: null,
+      loader: false,
+      hasError: false
+    };
+  },
+  methods: {
+    getMeetups() {
+      this.loader = true;
+      return fetchMeetupById(this.meetupId)
+        .then((result) => {
+          this.meetup = result;
+        })
+        .catch(error => {
+          this.hasError = true;
+        })
+        .finally(() => {
+          this.loader = false;
+        });
 
+    }
+  },
+  watch: {
+    meetupId() {
+      this.getMeetups()
+    }
+  },
+  mounted() {
+    this.getMeetups()
+  },
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+    <MeetupView :meetup="meetup" v-if="meetup" />
 
-      <UiContainer>
-        <UiAlert>Загрузка...</UiAlert>
-      </UiContainer>
+    <UiContainer v-if="loader">
+      <UiAlert>Загрузка...</UiAlert>
+    </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
-      </UiContainer>
-    </div>`,
+    <UiContainer v-if="hasError">
+      <UiAlert>error</UiAlert>
+    </UiContainer>
+    </div>
+  `
 });
